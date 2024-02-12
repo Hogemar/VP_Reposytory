ab52c6ffd891b54cede81c3389fd32d180b4f35a
Дмитрий <hotmangamer12567@gmail.com>
2024-02-12 23:52:59 +0300

    1st

---
+++ VP_Lab01/LogicPart/Logic.cs
@@ -0,0 +1,37 @@
+﻿namespace LogicPart
+{
+    public class Logic
+    {
+        // строка со всеми гласными
+        private static string vovels = "EeYyUuIiOoAa";
+        // строка со всеми согласными
+        private static string consonants = "QqWwRrTtPpSsDdFfGgHhJjKkLlZzXxCcVvBbNnMm";
+
+
+        public static float strAnalise(string? str)
+        {
+            if (str.Length == 0) 
+            {
+                throw new Exception("Ничего не введено!");
+            }
+
+            float percent = 0;
+            uint letterCount = 0;
+            foreach (var c in str)
+            {
+                if (vovels.Contains(c))
+                { 
+                    percent++;
+                    letterCount++;
+                }
+                if (consonants.Contains(c))
+                {
+                    letterCount++;
+                }
+
+            }
+
+            return percent*= 100F/(float)letterCount;
+        }
+    }
+}
---
+++ VP_Lab01/UserPart/Program.cs
@@ -0,0 +1,9 @@
+﻿using LP = LogicPart;
+
+Console.Write(" Введите строку латинских букв\n > ");
+Console.WriteLine($" Процент гласных = {LP.Logic.strAnalise(Console.ReadLine())}%");
+
+//for (int i = 0; i < 5000; i++)
+//{
+//    Console.SetWindowSize(Random.Shared.Next()%60+1, Random.Shared.Next()%40+1);
+//}
