0207f5e3ad58ff18b627bdf5f36846ea06d1010b
Дмитрий <hotmangamer12567@gmail.com>
2024-02-15 17:57:23 +0300

    Lab02 code

---
+++ VP_Lab02/Classes/Classes.cs
@@ -0,0 +1,178 @@
+﻿namespace Classes
+{
+    public class Student
+    {
+        public int Id { get; }              // Номер зачётной книжки
+        public string Name { get; }         // Имя
+        public string Surname { get; }      // Фамилия
+        public string MiddleName { get; }   // Отчество
+        public DateOnly BirthDate { get; }  // Дата рождения
+        public string address;              // Адрес
+        public char[] phoneNumber;          // Номер телефона
+
+        //public Student(int id)
+        //{
+        //    _id = id;
+        //}
+        //public Student(int id, string name, string surname, string middleName, DateOnly birthDate)
+        //{
+        //    _id = id;
+        //    Name=name;
+        //    Surname=surname;
+        //    MiddleName=middleName;
+        //    BirthDate=birthDate;
+        //}
+        public Student(int id, string name, string surname, DateOnly birthDate, string address, char[] phoneNumber, string middleName = null)
+        {
+            Id=id;
+            Name=name;
+            Surname=surname;
+            MiddleName=middleName;
+            BirthDate=birthDate;
+            this.address=address;
+            this.phoneNumber=phoneNumber;
+        }
+
+        public string GetFullName()
+        {
+            string fname = new string(Surname);
+            fname += ' ';
+            fname.Concat(Name);
+
+            if (MiddleName != null)
+            {
+                fname += ' ';
+                fname.Concat(Name);
+            }
+
+            return fname;
+        }
+
+        public override string ToString() 
+        {
+            string str = 
+                $"Зачётная книжка: {Id}\n" +
+                $"Имя: {Surname}\n" +
+                $"Фамилия: {Name}\n";
+
+            if (MiddleName != null) str +=
+                    $"Отчество: {MiddleName}\n";
+
+            str +=
+                $"Дата рождения: {BirthDate}\n" +
+                $"Адрес: {address}\n";
+
+            string phoneStr = new string("");
+            foreach(var c in phoneNumber)
+            {
+                phoneStr += c ;
+            }
+            
+            str+= $"Телефон: {phoneStr}\n";
+
+            return str;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null) return false;
+            if (obj == this) return true;
+
+            return (this.ToString().Equals(obj.ToString()));
+        }
+
+    }
+    public class Group
+    {
+        public int Number { get; }
+        private List<Student> _students = new List<Student>();
+
+        public Group(int groupNumber)
+        { 
+            this.Number = groupNumber; 
+        }
+
+        private void Sort()
+        {
+            for (int viewedN = 0; viewedN < _students.Count-1; viewedN++)
+            {
+                int max_k = viewedN;
+
+                for (int i = viewedN+1; i <  _students.Count; i++)
+                {
+                    if (_students[i].GetFullName().CompareTo(_students[max_k].GetFullName()) < 0)
+                        max_k = i;
+                }
+
+                Student studref = _students[viewedN];
+                _students[viewedN] = _students[max_k];
+                _students[max_k] = studref;
+
+                //swap(_students[viewedN], _students[max_k]);
+            }
+        }
+
+        public void Add(Student student)
+        {
+            _students.Add(student);
+            Sort();
+        }
+        public void Add(int id, string name, string surname, DateOnly birthDate, string address, char[] phoneNumber, string middleName = null)
+        {
+            _students.Add(new Student(id,name,surname, birthDate, address, phoneNumber, middleName));
+            Sort();
+        }
+
+        public bool Remove(int studentId)
+        {
+            foreach(var student in _students)
+            {
+                if(student.Id == studentId)
+                {
+                    _students.Remove(student);
+                    return true;
+                }
+            }
+            return false;
+        }
+        public bool Remove(Student student)
+        {
+            foreach (var groupStudent in _students)
+            {
+                if (groupStudent.Equals(student))
+                {
+                    _students.Remove(student);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public string GetInfo()
+        {
+            string info = new string("");
+            info += Number.ToString();
+            info += "\n\n";
+            foreach (var student in _students)
+            {
+                info += student.ToString();
+                info += "\n";
+            }
+
+            return info;
+        }
+
+        public Student? this[int id]
+        {
+            get
+            {
+                foreach (var student in _students)
+                {
+                    if (student.Id == id)
+                        return student;
+                }
+                return null;
+            }
+        }
+    }
+}
---
+++ VP_Lab02/ClassesUI/Program.cs
@@ -0,0 +1,19 @@
+﻿using Classes;
+using System.Numerics;
+
+Student a = new Student(15, "Артёльф", "Старопопов", new DateOnly(1998, 6, 23), "г. Воронеж, ул. Продольная, д. 16", ['7', '9', '5', '3', '8', '4', '7', '4', '2', '5', '2'], "Иванов");
+Student c = new Student(16, "Стасик", "Янчиков", new DateOnly(2000, 7, 12), "г. Махачкала, ул. Слюпа, д. 5", ['7', '9', '4', '2', '1', '3', '5', '1', '8', '2', '6'], "Изявич");
+Student b = new Student(10, "Бартёльф", "Астольфов", new DateOnly(1998, 6, 23), "г. Ульяновск, ул. Новая, д. 7", ['7', '9', '3', '7', '5', '2', '7', '3', '8', '2', '8'], "Крягович");
+
+//Console.WriteLine(a.Equals(b));
+//Console.WriteLine(a.Equals(c));
+
+
+Group group = new Group(340);
+group.Add(a);
+group.Add(c); 
+group.Add(b);
+
+Student studref = group[16];
+if(studref != null)
+    Console.WriteLine(studref.ToString());
