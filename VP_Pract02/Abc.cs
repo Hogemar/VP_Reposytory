a6694c7de9d96748fe3bd01ac86b4e9b71d3eca3
Дмитрий <hotmangamer12567@gmail.com>
2024-02-19 18:45:45 +0300

    + 2 практика

---
+++ VP_Pract02/Abc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class Round:Figure
+    {
+        public Round(string roundName = "", string roundColor = "") : base(roundName, roundColor) { }
+    }
+
+    public class Parallelogramm : Figure
+    {
+        public Parallelogramm(string parallelogrammName = "", string parallelogrammColor = "") : base(parallelogrammName, parallelogrammColor) { }
+    }
+
+    public class RegularnNgon:Figure
+    {
+        public RegularnNgon(string regularnNgonName = "", string regularnNgonColor = "") : base(regularnNgonName, regularnNgonColor) { }
+    }
+
+
+}
