7a904232412f4eed48b9f30b64e975ba7f2dbe09
Дмитрий <hotmangamer12567@gmail.com>
2024-02-21 18:30:25 +0300

    lab03 init (branch)

---
+++ VP_Lab03/Matrix/Matrix.cs
@@ -0,0 +1,9 @@
+﻿namespace MatrixLib
+{
+    public class Matrix
+    {
+        private double[,] elements;
+        public uint Rows { get; private set; }
+        public uint Columns { get; private set;}
+    }
+}
