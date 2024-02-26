4f6550de7e6505a585761366484b9944041838df
Дмитрий <hotmangamer12567@gmail.com>
2024-02-27 00:39:22 +0300

    pract 03 smth...

--- VP_Pract03/Programm/obj/Debug/net8.0/Programm.AssemblyInfo.cs
+++ VP_Pract03/Programm/obj/Debug/net8.0/Programm.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Programm")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6c962a6277dc4456855058cc0dde12817593304")]
 [assembly: System.Reflection.AssemblyProductAttribute("Programm")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Programm")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
