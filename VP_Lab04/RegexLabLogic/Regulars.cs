8b50ffb1c0defd279c9c114a2281f27b5ed1a7ca
Дмитрий <hotmangamer12567@gmail.com>
2024-03-03 00:20:00 +0300

    lab04 code beta

---
+++ VP_Lab04/RegexLabLogic/Regulars.cs
@@ -0,0 +1,102 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegexLabLogic
+{
+	public static class Regulars
+	{
+		// Подсчёт входящих почтовых индексов (с любыми разделителями)
+		public static int CountPostcodes(this string str)
+		{
+			return Regex.Count(str, @"\b\d{6}\b");
+		}
+
+		// Меняет местами первую и последнюю букву каждого слова
+		public static string WordToDorw(this string sentence)
+		{
+			var matches = Regex.Matches(sentence, @"\w+").ToArray(); // Получить все слова
+			var words = new string[matches.Length]; // Массив слов
+			var dorws = new string[matches.Length]; // Массив обработанных слов
+			for (int i = 0; i < matches.Length; i++)
+			{
+				words[i] = matches.GetValue(i).ToString(); // Получить слово
+				StringBuilder dorwSB = new(words[i]);
+				dorwSB[0] = words[i][words[i].Length-1]; // Поменять местами первую
+				dorwSB[words[i].Length-1] = words[i][0]; // и последнюю буквы
+				dorws[i] = dorwSB.ToString(); // Получить обработанное слово
+			}
+			
+			StringBuilder newSentenceSB = new(sentence);   // Изменённое предложение
+			for (int i = 0; i < words.Length; i++)
+			{
+				newSentenceSB.Replace(words[i], dorws[i]); // Замена слов
+			}
+
+			return newSentenceSB.ToString();
+		}
+
+		// Скрывает городскую часть номера со второй цифры
+		public static string HidePhonePart(this string str)
+		{
+			// Регулярное выражение для телефонов
+			string phonePattern = @"\+7 ((\(\d{3}\) \d{3})|(\(\d{4}\) \d{2})|(\(\d{5}\) \d{1}))-\d{2}-\d{2}";
+            // Строка со скрытыми номерами
+            StringBuilder hiddenSB = new(str); 
+			// Количество номеров
+			int phonesCount = Regex.Count(str, phonePattern);
+			for (int k = 0; k < phonesCount; k++) 
+			{
+                // Взять телефон
+                StringBuilder hiddenPhoneSB = new(Regex.Match(hiddenSB.ToString(), phonePattern).Value);
+                // Проход с конца
+                for (int i = hiddenPhoneSB.Length - 1; i >= 0; i--) 
+				{
+                    // Скрывать цифры
+                    if (hiddenPhoneSB[i] != '-')
+						hiddenPhoneSB[i] = 'x';
+                    // До первой цифры после пробела исключительно
+                    if (hiddenPhoneSB[i-2] == ' ') 
+						break;						
+				}
+				// Замена телефона на скрытый
+				hiddenSB.Replace(Regex.Match(hiddenSB.ToString(), phonePattern).Value, hiddenPhoneSB.ToString());
+			}
+
+			return hiddenSB.ToString();
+		}
+
+		// Возвращает входящие в строку автономера в виде последовательности с раделителем ';'
+		public static string GetLicensePlates(this string str)
+		{
+			// Регулярное выражение для автономеров
+			string platePattern = @"(([АВЕКМНОРСТУХ]{2} \d{3,4})|([АВЕКМНОРСТУХ]{2} \d{3,4} [АВЕКМНОРСТУХ]{1})|([АВЕКМНОРСТУХ]{2} \d{2} [АВЕКМНОРСТУХ]{2})|([АВЕКМНОРСТУХ]{1} ((\d{3} [АВЕКМНОРСТУХ]{2})|(\d{4} )))|(\d{3,4} [АВЕКМНОРСТУХ]{1,2})) \d{2,4}";
+
+			StringBuilder platesSB = new();
+			var plates = Regex.Matches(str, platePattern).ToArray();
+            foreach (var plate in plates)
+            {
+				platesSB.Append(plate.Value + "; ");
+            }
+
+			return platesSB.ToString();
+        }
+
+		// Возвращает строку, содержащую IPv4 адреса в десятичном формате из оригинальной строки,
+		// разделённые указанным разделителем
+		public static string GetIPv4Adresses(this string str, string separator = "; ")
+		{
+			// Регулярное выражение для IPv4
+			string adressPatern = @"\b(((2(([0-4][0-9])|5[0-5])|(1[0-9][0-9])|([1-9][0-9])|[0-9])\.){3}(2(([0-4][0-9])|5[0-5])|(1[0-9][0-9])|([1-9][0-9])|[0-9]))\b";
+
+            StringBuilder adressesSB = new();
+
+            var adresses = Regex.Matches(str, adressPatern).ToArray();
+            foreach (var adress in adresses)
+            {
+                adressesSB.Append(adress.Value + separator);
+            }
+
+            return adressesSB.ToString();
+        }
+    }
+}
---
+++ VP_Lab04/RegexLabUI/Program.cs
@@ -0,0 +1,47 @@
+﻿using RegexLabLogic;
+
+#region 1
+Console.WriteLine("\t--- Задание 1 ---\n\n");
+
+string postcodes = "146249 489214, 741367;412634 - 142014\r\n456124791465\r\n145621+462149-621367\\614657;513954,54\r\n165546\r\n236342.542345.345132.435634";
+Console.WriteLine($"{postcodes}\n\nЧисло почтовых индексов = {postcodes.CountPostcodes()}\n\n");
+#endregion
+
+#region 2
+Console.WriteLine("\t--- Задание 2 ---\n\n");
+
+string sentence = "Как только солнце заходит за горизонт," +
+    " начинается магия вечера - природа окутывается таинственным полумраком," +
+    " а небо наполняется мерцающими звездами:" +
+    " это время, когда сердце наполняется умиротворением и вдохновением.";
+
+Console.WriteLine($"{sentence}\n\n{sentence.WordToDorw()}\n\n");
+#endregion
+
+#region 3
+Console.WriteLine("\t--- Задание 3 ---\n\n");
+
+string phones = "+7 (495) 123-78-15\n+7 (8474) 45-78-97\n+7 (49112) 5-43-33";
+Console.WriteLine($"{phones}\n\n{phones.HidePhonePart()}\n\n");
+#endregion
+
+#region 4
+Console.WriteLine("\t--- Задание 4 ---\n\n");
+
+string licensePlates = "С 227 НА 94;В 555 РХ 095\nАО 365 789,УА 124 А 3547-АН 7331 47\n3146 УВ 64";
+
+Console.WriteLine($"{licensePlates}\n\n{licensePlates.GetLicensePlates()}\n\n");
+#endregion
+
+#region 5
+Console.WriteLine("\t--- Задание 5 ---\n\n");
+
+// Здесь 9 адресов и один неправильный
+string textWithAdresses = 
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at 192.168.1.1, ante." +
+    " Nulla facilisi. Integer nec 10.20.30.40 justo. Fusce 172.16.0.1 ac mauris auctor, 8.8.8.8 in commodo nisl." +
+    " Etiam tincidunt 255.255.255.0; turpis vel semper. Duis 123.45.67.89 vitae velit at 192.0.2.1 congue." +
+    " Phasellus 100.200.300.400 eget mauris id 172.31.255.254 dictum. Sed 1.2.3.4 vel turpis";
+
+Console.WriteLine($"{textWithAdresses}\n\n{textWithAdresses.GetIPv4Adresses("\n")}\n\n");
+#endregion
