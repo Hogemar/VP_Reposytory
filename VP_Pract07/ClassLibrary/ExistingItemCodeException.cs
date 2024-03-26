4114eeef498ae4a03f96bf36c9e474520196e185
Сафаров Д <saf.kyomail@gmail.com>
2024-03-26 21:24:40 +0300

    Pract07 done

--- VP_Pract07/ClassLibrary/ExistingItemCodeException.cs
+++ VP_Pract07/ClassLibrary/ExistingItemCodeException.cs
@@ -1,19 +1,32 @@
 ﻿namespace ClassLibrary
 {
+    [Serializable]
+
     public class ExistingItemCodeException : ApplicationException
     {
         public Item? Item 
         {
             get;
         }
-        public string? Data 
-        {
-            get; 
-        }
+
+
+
+        public ExistingItemCodeException() { }
+
+        public ExistingItemCodeException(string message) : base(message) { }
+
+        public ExistingItemCodeException(string message, Exception ex) : base(message) { }
+
+        protected ExistingItemCodeException(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext contex)
+            : base(info, contex) { }
 
         public ExistingItemCodeException(Item item) 
         {
-            Data=item.ToString();
+            base.Data.Add("Id", item.Id);
+            base.Data.Add("Name", item.Name);
+            base.Data.Add("Color", item.Color);
+            base.Data.Add("Price", item.Price);
+
             Item=item;
         }
     }
--- VP_Pract07/Programm/Program.cs
+++ VP_Pract07/Programm/Program.cs
@@ -7,25 +7,26 @@ Item item2 = new Item(1, "Кирпич", "Красный", 100.76);
 Item item3 = new Item(2, "Карандаш", "Черный", 50.11);
 Item item4 = new Item(2, "Стерка", "Синяя", 24.13);
 //1 case
-//try
-//{
-//    shop.addItem(item1);
-//    shop.addItem(item2);
-  
-//}
-//catch (ExistingItemCodeException ex)
-//{
-//    throw ex;
-//}
- 
-//2 case
 try
 {
-    shop.AddItem(item3);
-    shop.AddItem(item4);
+    shop.AddItem(item1);
+    shop.AddItem(item2);
+
 }
 catch (ExistingItemCodeException ex)
 {
+    throw ex;
     throw;
 }
+
+//2 case
+//try
+//    {
+//        shop.AddItem(item3);
+//        shop.AddItem(item4);
+//    }
+//    catch (ExistingItemCodeException ex)
+//    {
+//        throw;
+//    }
 
