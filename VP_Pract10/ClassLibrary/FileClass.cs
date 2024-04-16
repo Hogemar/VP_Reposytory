b2f49fb11d5f308f0b5b4de091bdad61e1d6c54d
Сафаров Д. <saf.kyomail@gmail.com>
2024-04-16 13:52:43 +0300

    pract 10 alpha

---
+++ VP_Pract10/ClassLibrary/FileClass.cs
@@ -0,0 +1,123 @@
+﻿namespace ClassLibrary
+{
+
+	public class FileClass : IDisposable
+    {
+        private FileStream? _stream;
+        private StreamReader? _reader;
+		private StreamWriter? _writer;
+        private bool _isDisposed = true;
+
+
+		private FileClass(string filePath, bool isNewFile)
+		{
+            if (CreateStream(filePath, isNewFile))
+                _isDisposed = false;
+		}
+		~FileClass()
+		{
+            Dispose();
+        }
+
+		public static FileClass Create(string filePath) => new FileClass(filePath, true);
+		public static FileClass Open(string filePath) => new FileClass(filePath, false);
+
+		public string? ReadLine()
+		{
+			if (_isDisposed) throw new Exception("Error: cannot read from file!");
+
+			return _reader.ReadLine();
+		}
+		public string Read(ushort count)
+		{
+			if (_isDisposed) throw new Exception("Error: cannot read from file!");
+
+			var buffer = new char[count];
+
+			if (_reader.Read(buffer, 0, Convert.ToInt32(count)) == 0)
+				return "";
+			else
+				return new string(buffer);
+		}
+		public void WriteLine(string str)
+		{
+            if (_isDisposed) throw new Exception("Error: cannot write to file!");
+
+			_writer.WriteLine(str);
+			_writer.Flush();
+        }
+        public void Write(string str, ushort count)
+		{
+            if (_isDisposed) throw new Exception("Error: cannot write to file!");
+
+            _writer.Write(str.Substring(0, Convert.ToInt32(count)));
+            _writer.Flush();
+        }
+        public void Dispose()
+        {
+            if (_isDisposed) return;
+
+            //Освобождаем неуправляемые ресурсы
+            if (_writer != null)
+            {
+                _writer.Flush();
+                _writer.Close();
+                _writer = null;
+            }
+
+            //Освобождаем неуправляемые ресурсы
+            if (_reader != null)
+            {
+                _reader.Close();
+                _reader = null;
+            }
+
+            //Освобождаем неуправляемые ресурсы
+            if (_stream != null)
+            {
+                _stream.Close();
+                _stream = null;
+            }
+
+            //Не отправлять объект в очередь финализации
+            GC.SuppressFinalize(this);
+
+            //Ресурсы были освобождены
+            _isDisposed = true;
+        }
+
+        private bool CreateStream(string filePath, bool isNewFile)
+        {
+            if (isNewFile)
+                _stream = new FileStream
+                    (
+                    filePath,
+                    FileMode.Create,
+                    FileAccess.ReadWrite,
+                    FileShare.ReadWrite | FileShare.Delete
+                    );
+            else
+                try
+                {
+                    _stream = new FileStream
+                        (
+                        filePath,
+                        FileMode.Open,
+                        FileAccess.ReadWrite,
+                        FileShare.ReadWrite | FileShare.Delete
+                        );
+                }
+                catch (FileNotFoundException)
+                {
+                    Console.WriteLine($"\n Error: file '{filePath}' not found!");
+                    return false;
+                }
+
+            _reader = new StreamReader(_stream);
+            _writer = new StreamWriter(_stream);
+
+            return true;
+        }
+    }
+
+}
---
+++ VP_Pract10/Program/Program.cs
@@ -0,0 +1,70 @@
+﻿using ClassLibrary;
+using static System.Net.Mime.MediaTypeNames;
+using System.Runtime.Intrinsics.X86;
+/* ПУНКТ 1*/
+
+string filePath = @"C:\test.txt";
+
+FileClass file = FileClass.Open(filePath);
+
+file = null;
+
+
+FileClass newFile = FileClass.Open(filePath);
+
+string line = newFile.ReadLine();
+Console.WriteLine(line);
+
+
+/*ПУНКТ 3*/
+
+//string filePath = @"C:\test.txt";
+//FileClass file = FileClass.Open(filePath);
+//file?.Dispose();
+//FileClass newFile = FileClass.Open(filePath);
+
+//string line = newFile.ReadLine();
+//Console.WriteLine(line);
+
+
+//FileClass file = null;
+//string filePath = @"C:\test2.txt";
+
+/*ПУНКТ 5*/
+//try
+//{
+//    file = FileClass.Open(filePath);
+//    file.WriteLine("Its new string");
+//}
+//finally
+//{
+//    file?.Dispose();
+//}
+
+/*ПУНКТ 6*/
+//using (FileClass file1 = FileClass.Open(filePath))
+//{
+//    string line = file1.ReadLine();
+//    Console.WriteLine(line);
+//}
+
+/*ПУНКТ 7*/
+//long startMemory = GC.GetTotalMemory(true);
+
+//Console.WriteLine("Начальный размер кучи: " + startMemory + " байт");
+
+//for (int i = 0; i < 3000; i++)
+//{
+//    new object();
+//}
+
+///*Получаем размер кучи после создания объектов*/
+//long afterAllocMemory = GC.GetTotalMemory(false);
+//Console.WriteLine("Размер кучи после выделения памяти для объектов: " + afterAllocMemory + " байт");
+
+///*Запускаем сборку мусора*/
+//GC.Collect();
+
+///*Получаем размер кучи после сборки мусора*/
+//long endMemory = GC.GetTotalMemory(true);
+//Console.WriteLine("Размер кучи после сборки мусора: " + endMemory + " байт");
