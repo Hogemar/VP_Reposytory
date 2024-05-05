931bd2a7101980386d17151aa9efdcdbac27c41a
Сафаров Д. <saf.kyomail@gmail.com>
2024-05-06 00:00:51 +0300

    pract 12 done

---
+++ VP_Pract12/VP_Pract12/Toster.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace VP_Pract12
+{
+    partial class Toster
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.breadTypeComboBox = new System.Windows.Forms.ComboBox();
+            this.startButton = new System.Windows.Forms.Button();
+            this.toastTimeTextBox = new System.Windows.Forms.TextBox();
+            this.remainingTimeLabel = new System.Windows.Forms.Label();
+            this.breadTypeLabel = new System.Windows.Forms.Label();
+            this.slicesLabel = new System.Windows.Forms.Label();
+            this.toastTimeLabel = new System.Windows.Forms.Label();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.slicesComboBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // breadTypeComboBox
+            // 
+            this.breadTypeComboBox.FormattingEnabled = true;
+            this.breadTypeComboBox.Location = new System.Drawing.Point(11, 28);
+            this.breadTypeComboBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.breadTypeComboBox.Name = "breadTypeComboBox";
+            this.breadTypeComboBox.Size = new System.Drawing.Size(108, 24);
+            this.breadTypeComboBox.TabIndex = 0;
+            // 
+            // startButton
+            // 
+            this.startButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.startButton.ForeColor = System.Drawing.Color.White;
+            this.startButton.Location = new System.Drawing.Point(11, 156);
+            this.startButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(108, 41);
+            this.startButton.TabIndex = 3;
+            this.startButton.Text = "Старт";
+            this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // toastTimeTextBox
+            // 
+            this.toastTimeTextBox.Location = new System.Drawing.Point(11, 108);
+            this.toastTimeTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.toastTimeTextBox.Name = "toastTimeTextBox";
+            this.toastTimeTextBox.Size = new System.Drawing.Size(108, 22);
+            this.toastTimeTextBox.TabIndex = 5;
+            // 
+            // remainingTimeLabel
+            // 
+            this.remainingTimeLabel.BackColor = System.Drawing.Color.Gray;
+            this.remainingTimeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.remainingTimeLabel.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.remainingTimeLabel.Location = new System.Drawing.Point(140, 156);
+            this.remainingTimeLabel.Name = "remainingTimeLabel";
+            this.remainingTimeLabel.Size = new System.Drawing.Size(128, 39);
+            this.remainingTimeLabel.TabIndex = 6;
+            this.remainingTimeLabel.Text = "Таймер";
+            // 
+            // breadTypeLabel
+            // 
+            this.breadTypeLabel.AutoSize = true;
+            this.breadTypeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.breadTypeLabel.Location = new System.Drawing.Point(132, 31);
+            this.breadTypeLabel.Name = "breadTypeLabel";
+            this.breadTypeLabel.Size = new System.Drawing.Size(211, 20);
+            this.breadTypeLabel.TabIndex = 7;
+            this.breadTypeLabel.Text = "🡄 Выбор вида хлебушка";
+            // 
+            // slicesLabel
+            // 
+            this.slicesLabel.AutoSize = true;
+            this.slicesLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.slicesLabel.Location = new System.Drawing.Point(132, 70);
+            this.slicesLabel.Name = "slicesLabel";
+            this.slicesLabel.Size = new System.Drawing.Size(212, 20);
+            this.slicesLabel.TabIndex = 8;
+            this.slicesLabel.Text = "🡄 Количество ломтиков";
+            // 
+            // toastTimeLabel
+            // 
+            this.toastTimeLabel.AutoSize = true;
+            this.toastTimeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.toastTimeLabel.Location = new System.Drawing.Point(132, 108);
+            this.toastTimeLabel.Name = "toastTimeLabel";
+            this.toastTimeLabel.Size = new System.Drawing.Size(185, 20);
+            this.toastTimeLabel.TabIndex = 9;
+            this.toastTimeLabel.Text = "🡄 Время жарки (сек.)";
+            // 
+            // stopButton
+            // 
+            this.stopButton.BackColor = System.Drawing.Color.Red;
+            this.stopButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.stopButton.ForeColor = System.Drawing.Color.White;
+            this.stopButton.Location = new System.Drawing.Point(11, 202);
+            this.stopButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(108, 41);
+            this.stopButton.TabIndex = 10;
+            this.stopButton.Text = "Стоп";
+            this.stopButton.UseVisualStyleBackColor = false;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // slicesComboBox
+            // 
+            this.slicesComboBox.FormattingEnabled = true;
+            this.slicesComboBox.Location = new System.Drawing.Point(11, 70);
+            this.slicesComboBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.slicesComboBox.Name = "slicesComboBox";
+            this.slicesComboBox.Size = new System.Drawing.Size(108, 24);
+            this.slicesComboBox.TabIndex = 11;
+            // 
+            // Toster
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(364, 261);
+            this.Controls.Add(this.slicesComboBox);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.toastTimeLabel);
+            this.Controls.Add(this.slicesLabel);
+            this.Controls.Add(this.breadTypeLabel);
+            this.Controls.Add(this.remainingTimeLabel);
+            this.Controls.Add(this.toastTimeTextBox);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.breadTypeComboBox);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Toster";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Toster";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox breadTypeComboBox;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.TextBox toastTimeTextBox;
+        private System.Windows.Forms.Label remainingTimeLabel;
+        private System.Windows.Forms.Label breadTypeLabel;
+        private System.Windows.Forms.Label slicesLabel;
+        private System.Windows.Forms.Label toastTimeLabel;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.ComboBox slicesComboBox;
+    }
+}
+
---
+++ VP_Pract12/VP_Pract12/Toster.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VP_Pract12
+{
+    public partial class Toster : Form
+    {
+        private int remainingTimeSeconds = 0;
+        private Timer timer = new Timer();
+        private bool isTimerRunning = false; // Флаг для отслеживания состояния таймера
+
+        public Toster()
+        {
+            InitializeComponent();
+            InitializeTimer();
+            InitializeComboBoxes();
+
+            // Заблокировать кнопку Старт при запуске
+            startButton.Enabled = true;
+        }
+
+        private void InitializeTimer()
+        {
+            timer.Interval = 1000; // Интервал в миллисекундах (1000 мс = 1 секунда)
+            timer.Tick += Timer_Tick; // Привязываем обработчик события Tick
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            remainingTimeSeconds--;
+
+            if (remainingTimeSeconds <= 0)
+            {
+                timer.Stop();
+                remainingTimeLabel.Text = "00:00:00";
+                startButton.Enabled = true; // Разблокировать кнопку Старт по истечении времени
+            }
+            else
+            {
+                // Обновляем Label с оставшимся временем
+                remainingTimeLabel.Text = TimeSpan.FromSeconds(remainingTimeSeconds).ToString(@"hh\:mm\:ss");
+            }
+        }
+
+        private void InitializeComboBoxes()
+        {
+            // Добавляем варианты в комбо-боксы
+            breadTypeComboBox.Items.AddRange(new object[] { "Белый", "Ржаной" });
+            slicesComboBox.Items.AddRange(new object[] { "1 ломтик", "2 ломтика" });
+
+            // Устанавливаем стиль комбо-бокса, чтобы пользователь не мог вводить текст
+            breadTypeComboBox.DropDownStyle = ComboBoxStyle.DropDownList;
+            slicesComboBox.DropDownStyle = ComboBoxStyle.DropDownList;
+
+            // Выбираем первый элемент по умолчанию
+            breadTypeComboBox.SelectedIndex = 0;
+            slicesComboBox.SelectedIndex = 0;
+        }
+
+        private async void startButton_Click(object sender, EventArgs e)
+        {
+            // Блокировать кнопку Старт при нажатии, пока идет таймер
+            startButton.Enabled = false;
+
+            // Считываем значения выбранных параметров
+            string breadType = breadTypeComboBox.SelectedItem.ToString();
+            int slicesCount = slicesComboBox.SelectedIndex + 1;
+            int toastTimeSeconds;
+
+            if (!int.TryParse(toastTimeTextBox.Text, out toastTimeSeconds) || toastTimeSeconds < 1 || toastTimeSeconds > 300)
+            {
+                MessageBox.Show("Пожалуйста, введите корректное время поджарки (от 1 до 300 секунд).", "Ошибка");
+                startButton.Enabled = true; // Разблокировать кнопку Старт при некорректных данных
+                return;
+            }
+
+            remainingTimeSeconds = toastTimeSeconds;
+            remainingTimeLabel.Text = TimeSpan.FromSeconds(remainingTimeSeconds).ToString(@"hh\:mm\:ss");
+
+            timer.Start();
+
+            // Имитация поджарки
+            await Task.Delay(toastTimeSeconds * 1000);
+
+            timer.Stop();
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Beep(1000, 250);
+                System.Threading.Thread.Sleep(100);
+            }
+
+            // Отображаем уведомление о завершении поджарки
+            string toastStatus = GetToastStatus(toastTimeSeconds);
+            MessageBox.Show($"Хлеб поджарен ({toastStatus})!", "Уведомление");
+
+            // Сброс выбранных параметров после завершения поджарки
+            breadTypeComboBox.SelectedIndex = 0;
+            slicesComboBox.SelectedIndex = 0;
+            toastTimeTextBox.Clear();
+            remainingTimeLabel.Text = "00:00:00";
+            startButton.Enabled = true; // Разблокировать кнопку Старт после завершения поджарки
+        }
+
+        private string GetToastStatus(int toastTimeSeconds)
+        {
+            // Определяем степень прожарки хлеба в зависимости от времени поджарки
+            if (toastTimeSeconds <= 60)
+            {
+                return "легкая прожарка";
+            }
+            else if (toastTimeSeconds <= 120)
+            {
+                return "средняя прожарка";
+            }
+            else if (toastTimeSeconds <= 180)
+            {
+                return "золотистая прожарка";
+            }
+            else if (toastTimeSeconds <= 240)
+            {
+                return "коричневая прожарка";
+            }
+            else
+            {
+                return "темная прожарка";
+            }
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            // Проверяем, запущен ли таймер перед его остановкой
+            if (timer.Enabled)
+            {
+                timer.Stop();
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.Beep(1000, 250);
+                    System.Threading.Thread.Sleep(100);
+                }
+
+                remainingTimeLabel.Text = "00:00:00";
+                startButton.Enabled = true; // Разблокировать кнопку Старт при нажатии на Стоп
+
+                // Получаем время поджарки из текстового поля
+                int toastTimeSeconds;
+                if (!int.TryParse(toastTimeTextBox.Text, out toastTimeSeconds) || toastTimeSeconds < 1 || toastTimeSeconds > 300)
+                {
+                    MessageBox.Show("Пожалуйста, введите корректное время поджарки (от 1 до 300 секунд).", "Ошибка");
+                    return;
+                }
+
+                // Получаем оставшееся время поджарки
+                int remainingSeconds = toastTimeSeconds - remainingTimeSeconds;
+
+                // Получаем степень прожарки на основе оставшегося времени поджарки
+                string toastStatus = GetToastStatus(remainingSeconds);
+
+                // Выводим соответствующее уведомление о прерванной поджарке
+                MessageBox.Show($"Поджарка прервана: достигнута степень прожарки '{toastStatus}'", "Уведомление");
+            }
+        }
+    }
+}
