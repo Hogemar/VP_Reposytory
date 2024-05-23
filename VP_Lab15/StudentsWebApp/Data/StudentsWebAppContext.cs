c97acf633981944e26ce9cb3324f3f29ea3fb45d
Сафаров Д. <saf.kyomail@gmail.com>
2024-05-23 18:59:50 +0300

    Lab 15 alpha

---
+++ VP_Lab15/StudentsWebApp/Data/StudentsWebAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using StudentsWebApp.Models;
+
+namespace StudentsWebApp.Data
+{
+    public class StudentsWebAppContext : DbContext
+    {
+        public StudentsWebAppContext (DbContextOptions<StudentsWebAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<StudentsWebApp.Models.Student> Student { get; set; } = default!;
+    }
+}
---
+++ VP_Lab15/StudentsWebApp/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace StudentsWebApp.Models
+{
+	public class Student
+	{
+		[Key]
+		public int Id { get; set; }
+
+		[Required]
+		[StringLength(100)]
+		public string Name { get; set; }
+
+		[Required]
+		[DataType(DataType.Date)]
+		public DateTime Birthdate { get; set; }
+
+		[Required]
+        [RegularExpression(@"\+7\(\d{3}\)\d{3}-\d{2}-\d{2}", ErrorMessage = "Phone number must be in the format +7(xxx)xxx-xx-xx")] 
+		public string Phone { get; set; }
+	}
+}
